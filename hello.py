b07025a01857aa6216e875ddc650a3385a744b6b
jw <choi.jae100@gmail.com>
2025-08-23 13:22:19 +0900

    add fastapi + htmx basic files

--- hello.py
+++ hello.py
@@ -1,5 +1,18 @@
+from mlx_lm import load, stream_generate
+
+
 def main():
-    print("Hello from llm-flashcard-maker!")
+    repo = "mlx-community/gemma-3-1b-it-bf16"
+    model, tokenizer = load(repo)
+
+    prompt = "Write a story about Einstein"
+
+    messages = [{"role": "user", "content": prompt}]
+    prompt = tokenizer.apply_chat_template(messages, add_generation_prompt=True)
+
+    for response in stream_generate(model, tokenizer, prompt, max_tokens=512):
+        print(response.text, end="", flush=True)
+    print()
 
 
 if __name__ == "__main__":
